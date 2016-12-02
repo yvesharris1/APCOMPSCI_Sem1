0bae4f80555da35581cafe82411294a4ec54618a
Yves Harris-Quanquin <yveshq@gmail.com>
2016-12-02 10:54:22 -0800

    added lesson 8 last lab

--- APLesson_08/TreeDeg60.java
+++ APLesson_08/TreeDeg60.java
@@ -1,23 +1,21 @@
 import java.util.Scanner;
 public class TreeDeg60
 {
-	static String word;
-	static int stop;
-	static int start;
 	public static void main(String[]args)
 	{
 		Scanner kb = new Scanner(System.in);
 		System.out.println("Please enter a word: ");
-		word = kb.nextLine();
-		stop = word.length();
-		System.out.println(tree(word, start, stop));
+		String word = kb.nextLine();
+		int stop = word.length();
+		tree(word, 0, stop);
 	}
-	public static String tree(String word, int start, int stop)
+	public static void tree(String word, int start, int stop)
 	{
-		if (start <= stop)
-			System.out.printf()
-			start ++;
+		if (start <= stop){
+			System.out.printf("%20s\n", word.substring(0, start));
+			start++;
 			tree(word, start, stop);
-			
+		}
+		
 	}
 }
